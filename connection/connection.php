4462b9da47267d1498bf64e96282787a7b3aa2c9
Álvaro Santos <129607745+alvarodavisantos@users.noreply.github.com>
2023-08-31 21:58:36 -0300

    Create connection.php

---
+++ connection/connection.php
@@ -0,0 +1,19 @@
+<?php
+ $hostname = 'localhost';
+ $username = 'root';
+ $password = '';
+ $database = 'cadastro_onubus';
+    
+   $connection = mysqli_connect($hostname, $username, $password, $database) or die("Não foi possível conectar");
+  
+try {
+    // cria o objeto PDO de conexão com o servidor de banco de dados 
+    $conn = new PDO("mysql:host=$hostname;dbname=$database;charset=utf8", $username, $password,
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    // configura os tipos de erros a serem mostrados pelo exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //    echo 'Conexao efetuada com sucesso!';
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+?>
